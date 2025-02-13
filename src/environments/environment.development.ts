8e2d6b90e4d0c0c2ab2fcc92376efd27047c4384
Boukhriss mohamed <booukhriss@gmail.com>
2025-02-13 09:35:48 +0100

    fix /  on endpoint link

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   liveKitEndpoint: "ws://localhost:7880",
-  tokenGeneratorEndpoint: "https://livekit-token-generator-5tcn.onrender.com/",
+  tokenGeneratorEndpoint: "https://livekit-token-generator-5tcn.onrender.com",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   liveKitEndpoint: "wss://livekit-server-keq6.onrender.com",
-  tokenGeneratorEndpoint: "https://livekit-token-generator-5tcn.onrender.com/",
+  tokenGeneratorEndpoint: "https://livekit-token-generator-5tcn.onrender.com",
 };
